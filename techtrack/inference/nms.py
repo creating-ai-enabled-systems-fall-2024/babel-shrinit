7f14fe3e0db0760b26f60de94395dcee7d264a7f
Shrinit Babel <152827666+shrinitbabel@users.noreply.github.com>
2024-11-05 09:27:45 -0500

    Update nms.py

--- techtrack/inference/nms.py
+++ techtrack/inference/nms.py
@@ -2,18 +2,18 @@ import cv2
 
 def filter(bboxes, class_ids, scores, nms_iou_threshold):
     """
-    Filters bounding boxes using the Non-Maximum Suppression (NMS) algorithm.
+    Filters bounding boxes using the Non-Maximum Suppression (NMS)
 
     Parameters:
-    - bboxes (list of tuples): Bounding boxes in the format [(x, y, width, height), ...].
-    - class_ids (list of ints): Class IDs corresponding to each bounding box.
-    - scores (list of floats): Confidence scores for each bounding box.
-    - nms_iou_threshold (float): Intersection over Union (IoU) threshold for NMS.
+    - bboxes (list of tuples): Bounding boxes in the format
+    - class_ids (list of ints): Class IDs corresponding 
+    - scores (list of floats): Confidence scores 
+    - nms_iou_threshold (float): Intersection over Union (IoU) threshold for NMS
 
     Returns:
-    - filtered_bboxes (list of tuples): Bounding boxes after NMS.
-    - filtered_class_ids (list of ints): Class IDs after NMS.
-    - filtered_scores (list of floats): Confidence scores after NMS.
+    - filtered_bboxes (list of tuples): Bounding boxes after NMS
+    - filtered_class_ids (list of ints): Class IDs after NMS
+    - filtered_scores (list of floats): Confidence scores after NMS
     """
     # Convert bboxes to the format required by cv2.dnn.NMSBoxes (i.e., [x, y, w, h])
     boxes = [list(map(int, bbox)) for bbox in bboxes]
